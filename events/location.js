a9d52817977c460558a746f2f7e3cba36895bbe6
keelbeelveel <k.belveal17@gmail.com>
2020-08-17 14:05:20 -0400

    lol

--- events/location.js
+++ events/location.js
@@ -1,20 +1,54 @@
-// Script modified: Mon August 17, 2020 @ 12:47:22 EDT
+// Script modified: Mon August 17, 2020 @ 01:59:48 EDT
 const express = require('express');
 const heartbeat = require('../heartbeat');
 const location = heartbeat.devices.location;
 const logger = require('../logger');
 const router = express.Router();
 
+router.post('/life360/arriveHome', async (req, res) => {
+    logger.debug("[Events/Location] POST to '/life360/arriveHome'");
+    try {
+        location.life360.updateStatusKey('atHome', true);
+        res.status(200).send("Successfully updated location").end();
+    } catch (err) {
+        logger.error("[Events/Location] Error:");
+        logger.error(`> ${err}`);
+        res.status(400).send(`Bad request: ${err}`).end();
+    }
+});
 router.post('/life360/allNotHome', async (req, res) => {
     logger.debug("[Events/Location] POST to '/life360/allNotHome'");
     try {
-    location.forEach(device => {
-        device.updateStatusKey('location', 'notHome');
-    }); 
-        res.status(200).send("Successfully updated location");
+        location.life360.updateStatusKey('atHome', false);
+        res.status(200).send("Successfully updated location").end();
     } catch (err) {
-        res.status(400).send(`Bad request: ${err}`);
+        logger.error("[Events/Location] Error:");
+        logger.error(`> ${err}`);
+        res.status(400).send(`Bad request: ${err}`).end();
     }
 });
+router.post('/life360/arriveShanty', async (req, res) => {
+    logger.debug("[Events/Location] POST to '/life360/arriveShanty'");
+    try {
+        location.life360.updateStatusKey('atShanty', true);
+        res.status(200).send("Successfully updated location").end();
+    } catch (err) {
+        logger.error("[Events/Location] Error:");
+        logger.error(`> ${err}`);
+        res.status(400).send(`Bad request: ${err}`).end();
+    }
+});
+router.post('/life360/allNotShanty', async (req, res) => {
+    logger.debug("[Events/Location] POST to '/life360/allNotShanty'");
+    try {
+        location.life360.updateStatusKey('atShanty', false);
+        res.status(200).send("Successfully updated location").end();
+    } catch (err) {
+        logger.error("[Events/Location] Error:");
+        logger.error(`> ${err}`);
+        res.status(400).send(`Bad request: ${err}`).end();
+    }
+});
+
 
 module.exports = router;
--- heartbeat.js
+++ heartbeat.js
@@ -1,4 +1,4 @@
-// Script modified: Mon August 17, 2020 @ 12:47:22 EDT
+// Script modified: Mon August 17, 2020 @ 02:01:56 EDT
 const fs = require('fs');
 const logger = require('./logger');
 //const path = require('path');
@@ -76,10 +76,7 @@ var deviceList = {
         side: new Device('garageSideDoor', { position: undefined })
     },
     location: {
-        emma:       new Device('locationEmma',      { location: undefined }),
-        jennifer:   new Device('locationJennifer',  { location: undefined }),
-        kyle:       new Device('locationKyle',      { location: undefined }),
-        scott:      new Device('locationScott',     { location: undefined })
+        life360: new Device('life360', { atHome: undefined, atShanty: undefined })
     }
 };
 
